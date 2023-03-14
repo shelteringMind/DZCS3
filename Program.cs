a82afdc13dab4478cde50f4999d199e549e6f00f
Anton Kruglyakov <akrugs@mail.ru>
2023-03-15 01:36:56 +0500

    Задача 19

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//*/
+//Задача 19
+
+string ReadNum(string text)
+{
+    System.Console.Write(text);
+    return Console.ReadLine()!;
+}
+
+void pаlindrom(string number)
+{
+    for (int i = 0; i <= number.Length / 2; i++)
+    {
+        if (number[i] != number[number.Length - i - 1])
+        {
+            Console.Write("Введенное число не является палиндромом!");
+            return;
+        }
+    }
+    Console.Write($"{number} является палиндромом!");
+    return;
+}
+
+string pal = ReadNum("Введите пятизначное число: ");
+if (pal.Length == 5)
+{
+    pаlindrom(pal);
+}
+else
+{
+    Console.Write("Вы ввели число, отличающееся от требуемого!");
+}
